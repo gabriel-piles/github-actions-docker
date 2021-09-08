dc96d6f44f264584a76e9b195ca5099f350327c9
gabo <gabriel.piles.glez@gmail.com>
2021-09-08 11:25:44 +0200

    Try adding logger

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from data import Data
+from get_logger import get_logger
 
 app = FastAPI()
+
+logger = get_logger()
 
 
 @app.get('/')
 async def info():
+    logger.info('info')
     return Data(paragraphs=['a']).json()
